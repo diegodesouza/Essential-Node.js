9d0f75f6878f85594e3a22a3a44d8ba6ea59b96e
Diego de Souza <diegotrus7@gmail.com>
2015-04-09 19:18:39 -0400

    Handling GET requests returning JSON

--- Chapter05/airline/routes/index.js
+++ Chapter05/airline/routes/index.js
@@ -3,6 +3,28 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+var flight = require('../flight');
+
+var flight1 = flight({
+	number: 1,
+	origin: 'LAX',
+	destination: 'DCA',
+	departs: '9AM',
+	arrives: '4PM'
+});
+
+var flight2 = flight({
+	number: 2,
+	origin: 'LAX',
+	destination: 'PDX',
+	departs: '10AM',
+	arrives: '12PM'
+});
+
+exports.flight1 = function(req, res){
+  res.jsonr(flight1.getInformation());
+};
+
+exports.flight2 = function(req, res){
+  res.jsonr(flight2.getInformation());
 };
